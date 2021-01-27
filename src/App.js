96b93a0cee47aaddce44199fea13d83415396d36
Your Name <you@example.com>
2021-01-27 09:09:10 -0600

    Refactor code use hooks

--- src/App.js
+++ src/App.js
@@ -1,38 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import { CardList } from './components/card-list/CardList';
 import {SearchBox} from './components/search-box/SearchBox';
 
-class App extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-     monsters: [],
-     searchField: ''
-    } 
-  }
+function App() {
+  const [monsters, setMonsters] = useState([]);
+  const [searchField, setSearchField] = useState('');
 
-  componentDidMount() {
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
     .then(res => res.json() )
-    .then(users => this.setState({monsters: users}) )
-  }
+    .then(users => setMonsters(users))
+  }, []);
 
-  handleChange = (e) => {
-    this.setState({searchField: e.target.value})
-  }
-    render() {
-      const {monsters, searchField} = this.state;
+   const  handleChange = (e) =>  {
+    setSearchField(e.target.value)
+    }
       const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
       return (
         <div className="App">
         <h1>Monsters Rolodex</h1>
         <SearchBox 
-        handleChange={this.handleChange} 
+        handleChange={handleChange} 
         placeholder='Search Monsters...'  />
         <CardList monsters={filteredMonsters} />
        </div>
       );
-    }} 
+    }
 
 export default App;
